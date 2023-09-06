e4c288f2daf45b1ba232f94c6cb9e4330cba8535
SemiBite <semibite@users.noreply.github.com>
2023-09-06 13:37:24 +0900

    Create Project

---
+++ Source/BackFox4/BackFox4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BackFox4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BackFox4, "BackFox4" );
---
+++ Source/BackFox4/BackFox4GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BackFox4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BACKFOX4_API ABackFox4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
